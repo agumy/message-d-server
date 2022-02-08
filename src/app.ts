44f6b4d30548fdd36b47a3a5ee6f931667fa1af0
agumy <nov13sh@gmail.com>
2022-02-08 10:51:39 +0900

    premium

--- src/app.ts
+++ src/app.ts
@@ -25,10 +25,17 @@ server.post<{
   queries.append("auth_key", body.auth_key);
   queries.append("target_lang", body.target_lang);
   queries.append("tag_handling", "xml");
-  // queries.append("ignore_tags", "pre,code");
+  queries.append("ignore_tags", "code,pre");
   queries.append("split_sentences", "nonewlines");
   queries.append("non_splitting_tags", "code,a");
-  const res = await fetch("https://api-free.deepl.com/v2/translate", {
+  // const res = await fetch("https://api-free.deepl.com/v2/translate", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //   },
+  //   body: queries,
+  // });
+  const res = await fetch("https://api.deepl.com/v2/translate", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
